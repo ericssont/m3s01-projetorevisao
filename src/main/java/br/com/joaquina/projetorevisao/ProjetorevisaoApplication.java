e2bb4c86728b49c2ec0bb4d1174d9460391aebee
Ericsson Tribess <ericktribess@gmail.com>
2023-05-03 21:19:31 -0300

    first commit

---
+++ src/main/java/br/com/joaquina/projetorevisao/ProjetorevisaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaquina.projetorevisao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetorevisaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetorevisaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/joaquina/projetorevisao/ProjetorevisaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaquina.projetorevisao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetorevisaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
