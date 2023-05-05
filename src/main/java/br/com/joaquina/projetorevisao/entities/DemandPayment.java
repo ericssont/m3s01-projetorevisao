4b6e65c79b68344616d29c50b04e00fcbf597177
Ericsson Tribess <ericktribess@gmail.com>
2023-05-04 21:14:52 -0300

    +classes

---
+++ src/main/java/br/com/joaquina/projetorevisao/entities/DemandPayment.java
@@ -0,0 +1,35 @@
+package br.com.joaquina.projetorevisao.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class DemandPayment {
+
+
+    @Id
+    @Column(name = "demand_payment_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Demand demand;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private PaymentMethod paymentMethod;
+
+    @Column(length = 30)
+    private String cardNumber;
+
+    @Column(length = 50)
+    private String cardHolder;
+    @Column(length = 7)
+    private String cardThru;
+
+    private Integer cardVerification;
+
+
+}
